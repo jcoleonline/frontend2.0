860d9a1acb04052b0cd65b1fcc944e4f088f309d
Ramon Santana <jcoleonlinellc@gmail.com>
2023-02-16 19:33:43 -0600

    ramon tuesday

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//Ramon 1
+let searchButton = document.getElementById("button-addon2");
+let searchInput = document.getElementById("search-bar");
+
+searchButton.addEventListener('click', () => {
+    let name = searchInput.value;
+    let finalUrl = `https://restcountries.com/v3.1/name/${name}?fullText=true`;
+
+    console.log(finalUrl);
+});
