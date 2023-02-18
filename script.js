58e2df5413317d2d2b3bbe49c4ca75d54804cf8b
Ramon Santana <jcoleonlinellc@gmail.com>
2023-02-18 12:38:14 -0600

    drop-down menu for countries

--- script.js
+++ script.js
@@ -1,9 +1,109 @@
-let searchButton = document.getElementById("button-addon2");
-let searchInput = document.getElementById("search-bar");
+const inputEl = document.querySelector("#autocomplete-input")
 
-searchButton.addEventListener('click', () => {
-    let name = searchInput.value;
-    let finalUrl = `https://restcountries.com/v3.1/name/${name}?fullText=true`;
+inputEl.addEventListener("input", onInputChange);
 
-    console.log(finalUrl);
-})
+getCountryData();
+
+let countryNames = [];
+
+//--
+
+async function getCountryData() {
+  const countryRes = await fetch("https://restcountries.com/v3.1/all");
+  const data = await countryRes.json();
+
+  countryNames = data.map((country) => {
+    return country.name.common;
+  });
+}
+
+//-----
+
+function onInputChange() {
+  removeAutocompleteDropdown();
+
+  const value = inputEl.value.toLowerCase();
+
+  if(value.length === 0)return;
+
+  const filteredNames = [];
+
+  countryNames.forEach((countryName) => {
+    if (countryName.substr(0, value.length).toLowerCase() === value)
+      filteredNames.push(countryName);
+  });
+  createAutocompleteDropdown(filteredNames);
+}
+
+//-----
+
+function createAutocompleteDropdown(List) {
+  const listEl = document.createElement("ul");
+  listEl.className = "autocomplete-list";
+  listEl.id = "autocomplete-list";
+
+  List.forEach((country) => {
+    const listItem = document.createElement("li");
+
+    const countryButton = document.createElement("button");
+    countryButton.innerHTML = country;
+    countryButton.addEventListener('click', onCountryButtonClick);
+    listItem.appendChild(countryButton);
+
+    listEl.appendChild(listItem);
+  });
+
+  document.querySelector("#autocomplete-wrapper").appendChild(listEl);
+};
+
+//----
+
+function removeAutocompleteDropdown() {
+  const listEl = document.querySelector('#autocomplete-list')
+  if (listEl) listEl.remove();
+};
+
+//---
+
+function onCountryButtonClick(e) {
+e.preventDefault();
+
+const buttonEl = e.target;
+inputEl.value = buttonEl.innerHTML;
+
+removeAutocompleteDropdown();
+
+}
+
+//----
+
+
+
+//----------------
+
+// let searchButton = document.getElementById("button-addon2");
+// let searchInput = document.getElementById("search-bar");
+
+// searchButton.addEventListener('click', () => {
+    
+//     let name = searchInput.value;
+//     let finalUrl = `https://restcountries.com/v3.1/name/${name}?fullText=true`;
+
+//     console.log(finalUrl);
+//     fetch(finalUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         console.log(data[0]);
+//         console.log(data[0].capital[0]);
+//         console.log(data[0].flags.svg);
+//         console.log(data[0].name.common);
+//         console.log(data[0].continents[0]);
+//         console.log(Object.keys(data[0].currencies)[0]);
+//         console.log(data[0].currencies[Object.keys(data[0].currencies)]);
+//         console.log();
+
+//     });
+// });
+
+
+
